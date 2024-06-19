aa7a5b7b1c9b96803fa9f7246954485510224376
unknown <govindbaral89@gmail.com>
2024-06-19 17:21:15 +0545

    Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
-
+import Footer from "@/components/Common/Footer";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
