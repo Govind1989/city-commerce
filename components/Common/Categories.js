d1d327a76e6322adf842b471e97812ad76166e9b
unknown <govindbaral89@gmail.com>
2024-06-25 15:53:15 +0545

    Categories carousel.

--- components/Common/Categories.js
+++ components/Common/Categories.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 const Categories = () => {
   const [isHovered, setIsHovered] = useState(false);
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [direction, setDirection] = useState("right");
   const carouselRef = useRef(null);
   const ulRef = useRef(null);
   const intervalIdRef = useRef(null);
@@ -16,15 +17,24 @@ const Categories = () => {
     const startCarousel = () => {
       intervalIdRef.current = setInterval(() => {
         setScrollPosition((prevPosition) => {
-          const newPosition = prevPosition + 1;
-          ul.style.transform = `translateX(-${newPosition}px)`;
-          if (newPosition >= ul.scrollWidth / 2) {
-            ul.style.transform = "translateX(0)";
-            return 0;
+          let newPosition = prevPosition;
+
+          if (direction === "right") {
+            newPosition += 1;
+            if (newPosition >= ul.scrollWidth - carousel.offsetWidth) {
+              setDirection("left");
+            }
+          } else {
+            newPosition -= 1;
+            if (newPosition <= 0) {
+              setDirection("right");
+            }
           }
+
+          ul.style.transform = `translateX(-${newPosition}px)`;
           return newPosition;
         });
-      }, 50);
+      }, 10); // Adjust the interval speed as needed
     };
 
     if (!isHovered) {
@@ -42,20 +52,39 @@ const Categories = () => {
       setIsHovered(false);
       carousel.classList.remove("custom-hr-scroll");
       carousel.style.overflowX = "hidden";
+      startCarousel();
+    };
+
+    const handleScroll = () => {
+      clearInterval(intervalIdRef.current);
+      const currentScrollPosition = carousel.scrollLeft;
+      setScrollPosition(currentScrollPosition);
+      if (currentScrollPosition === 0) {
+        setDirection("right");
+        ul.style.transform = `translateX(0px)`;
+      } else if (
+        currentScrollPosition >=
+        ul.scrollWidth - carousel.offsetWidth
+      ) {
+        setDirection("left");
+        ul.style.transform = `translateX(0px)`;
+      }
     };
 
     carousel.addEventListener("mouseenter", handleMouseEnter);
     carousel.addEventListener("mouseleave", handleMouseLeave);
+    carousel.addEventListener("scroll", handleScroll);
 
     return () => {
       clearInterval(intervalIdRef.current);
       carousel.removeEventListener("mouseenter", handleMouseEnter);
       carousel.removeEventListener("mouseleave", handleMouseLeave);
+      carousel.removeEventListener("scroll", handleScroll);
     };
-  }, [isHovered]);
+  }, [isHovered, direction]);
 
   return (
-    <div className="flex flex-col justify-center">
+    <div className="flex flex-col justify-start">
       <div className="w-full h-full mx-auto">
         <div className="">
           <div
@@ -65,7 +94,7 @@ const Categories = () => {
           >
             <ul
               ref={ulRef}
-              className="flex gap-4 relative items-start justify-center md:justify-start mb-2"
+              className="flex gap-4 relative items-start justify-start mb-2"
             >
               <div className="min-w-fit w-full shadow-md rounded-lg flex flex-col md:flex-row justify-between relative">
                 <img
